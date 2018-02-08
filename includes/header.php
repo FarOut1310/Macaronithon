c2ee32927c4637919b56ceec4e744e8ecee2b83c
Linas Mackonis <linas.mackonis@gmail.com>
2018-02-08 12:42:16 +0100

    Linas first commit

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header class="macaronithon">
+	<h1 class="macaronithon--header"><span class="macaronithon--header-span1">Macaroni</span><span class="macaronithon--header-span2">thon</span></h1>
+</header>
