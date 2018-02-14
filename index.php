7ad2481b90ec400de33634ddddc3faa4ec4e139a
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-02-14 14:41:49 +0100

    Pictures, HTML and CSS, Animation, html to PHP
    Added new pictures and deleted  old ones.
    
    Deleted some HTML so that the new animations could work properly.
    
    Added, deleted and structured CSS. Added new animations to the predator, the human and the logo.  Added in some of Linas HTML and CSS to the search bar, but not all yet.
    
    Edited my index.html to index.php so that i can work with the other pages.

--- index.php
+++ index.php
@@ -1,47 +1,73 @@
-<?php require 'includes/head.php'; ?>
-<?php require 'includes/header.php'; ?>
-<!--Popup-->
-<span class="window--alert none conditionalClose "><p>Unfortunately we don't have your product. Do you want to add it?</p>
-		<div class="grid-container--buttons">
-		<div class="button button--ok">OK</div>
-		<div class="button button--cancel">CANCEL</div>
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+	<!-- Title -->
+	<title>Macaronithon - How far can you run before you get eaten.</title>
+
+	<!-- Description -->
+	<meta name="description" content="Macaronithon is an online web application where you can see how far you can run on different kinds of food. Just made in a new and fun way!">
+
+	<!-- Keywords -->
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+
+	<!-- Viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+
+
+	<!-- CSS & SASS -->
+	<link rel="stylesheet" type="text/css" href="sass/styles.scss">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+<body>
+
+	<!-- Grid start -->
+<div class="grid-container">
+
+	<!-- Header if needed 
+	<header>
+	</header>
+	-->
+
+	<!-- Front page logo/name -->
+	<div class="item1">
+        <div class="animated bounceInDown">
+        	<h1 id="frontname">Macaronithon(insert logo)</h1>
+			<p id="fronttext">How far can you run before you get eaten</p>
 		</div>
-		</span>
-		
-<main class="mainArea1">
-	
-	<section class="mainArea1--searchField">
+	</div>
 
-		<div class="mainArea1--searchField-left"></div>
-		<div class="mainArea1--searchField-center">
-			<input onclick="getLowercase()" class="awesomplete inputMeal" placeholder="Search for the meal" data-list="pizza, hamburger">
-			<button class="submitMeal"><span>Go!</span></button>
-		</div>
-		<div class="mainArea1--searchField-right"></div>
-	</section>
+	<!-- Search bar -->
+	<div class="item2">
+		<input type="text" placeholder="Search..">
+	</div>
 
-	<section class="mainArea1--background">
-		<div class="mainArea1--background-left"><img id="predatorrex" src="img/predatorrex.png" alt="Predator" width="300px"></div>
-		<div class="mainArea1--background-center"></div>
-		<div class="mainArea1--background-right"><img id="humanrun" src="img/humanrun.png" alt="Predator" width="200px"></div>
-		<div class="mainArea1--background-bottom"></div>
-		
-	</section>
+	<!-- Pictures of the predator and human -->
+	<div class="item3">
+        <div class="animated tada">
+			<img src="images/predatorrex.png" alt="Predator hunting running human" title="T-rex" class="animated zoomIn t-rex">
+        </div>
+	</div>
 
-</main>
-<script>
-	function getLowercase() {
-		var lowercaseValue = document.getElementsByClassName("inputMeal")[0].value;
-		var res = str.toLowerCase();
-	}
-</script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="js/awesomplete.min.js"></script>
-<script src="js/getmeal.js" type="text/javascript"></script>
-<script src="js/popup.js" type="text/javascript"></script>
+	<div class="item4">
+        <div class="animated shake">
+    		<img src="images/humanrun.png" alt="Human running from predator" title="Human" class="animated zoomIn jello human">
+        </div>
+	</div>
 
-<?php include 'includes/footer.php'; ?>
+	<!-- Footer if needed-->
+	<footer>
+		<!-- I was thinking social media link or something maybe -->
+	</footer>
 
-
-
-
+</div>
+	<!-- Scripts -->
+	<script src="#"></script>
+</body>
+</html>
