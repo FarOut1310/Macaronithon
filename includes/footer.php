da394f00e6f0fb66e7b29a6ddea8c964f5293fcf
Linas Mackonis <linas.mackonis@gmail.com>
2018-02-15 09:53:29 +0100

    Implemented php copyright block

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,15 @@
-<footer class="footer">FOOTER</footer>
+<?php 
+$startYear = 2018;
+$thisYear = date('Y');
+if ($thisYear > $startYear) {
+    $thisYear = date('y');
+    $copyright = "$startYear&dash;$thisYear";
+} else {
+    $copyright = $startYear;
+}
+ ?>
+<footer class="footer"><p class="footer--copyright" role="contentinfo">© <?php echo $copyright ?> Macaronithon</p>
+</footer>
 
 </body>
 
